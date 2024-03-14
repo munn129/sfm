073d1f832bdd8b5270c193182350abc10e6f53d9
moon <ans92175@gmail.com>
2024-03-14 17:16:58 +0900

    add data

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('./Data/0000.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img1 = cv2.imread('./data/0000.JPG', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('./data/0001.JPG', cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow(gray)
+sift = cv2.SIFT_create()
+keypoint1, descriptor1 = sift.detectAndCompute(img1, None)
+keypoint2, descriptor2 = sift.detectAndCompute(img1, None)
+
+# cv2.imshow('d', img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
