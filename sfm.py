77ed158c421c9cf882d306d2d3e7166cdfcdc836
moon <ans92175@gmail.com>
2024-03-15 21:39:53 +0900

    use triangulation, but core bump is occured

--- sfm.py
+++ sfm.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 
-from calibrated_fivepoint import calibrated_fivepoint
+
+# from calibrated_fivepoint import calibrated_fivepoint
 
 filename = './data/K.txt'
 K = []
@@ -38,17 +39,35 @@ for i, (m,n) in enumerate(matches):
 
 pts1 = np.int32(pts1)
 pts2 = np.int32(pts2)
-# F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
-E, mask = cv2.findEssentialMat(pts1, pts2, K)
-EE = calibrated_fivepoint(pts1, pts2)
 
-r1, r2, t = cv2.decomposeEssentialMat(E)
-retval, rot, tran, m = cv2.recoverPose(E, pts1, pts2)
+E, mask = cv2.findEssentialMat(pts1, pts2, K, method=cv2.RANSAC)
 
 pts1 = pts1[mask.ravel()==1]
 pts2 = pts2[mask.ravel()==1]
- 
-# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None)
-# cv2.imshow('d', img3)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+
+# r1, r2, t = cv2.decomposeEssentialMat(E)
+retval, R, t, m = cv2.recoverPose(E, pts1, pts2)
+
+Rt0 = np.hstack((np.eye(3), np.zeros((3,1))))
+Rt1 = np.hstack((R,t))
+Rt1 = np.matmul(K, Rt1)
+
+tri_pt1 = np.transpose(pts1)
+tri_pt2 = np.transpose(pts2)
+
+p3ds = []
+
+for pt1, pt2 in zip(tri_pt1, tri_pt2):
+    p3d = cv2.triangulatePoints(Rt0, Rt1, tri_pt1, tri_pt2)
+    p3d /= p3d[3]
+    p3ds.append(p3d)
+
+p3ds = np.array(p3ds).T
+
+X = np.array([])
+Y = np.array([])
+Z = np.array([])
+
+X = np.concatenate((X, p3ds[0]))
+Y = np.concatenate((Y, p3ds[1]))
+Z = np.concatenate((Z, p3ds[2]))
