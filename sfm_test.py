b7b8001a346de77f3c1bab99a115db021b62e48b
moon_home <ans92175@gmail.com>
2024-03-23 13:18:54 +0900

    refactoring

--- sfm_test.py
+++ sfm_test.py
@@ -17,18 +17,30 @@ def load_K(K_path) -> list:
             K.append(row)        
     return np.array(K)
 
-def sift_des_generator(img1, img2):
+def sift_matcher(img1, img2) -> tuple:
     sift = cv2.SIFT_create()
 
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    query_kp, query_des = sift.detectAndCompute(img1, None)
+    train_kp, train_des = sift.detectAndCompute(img2, None)
 
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
+    matches = bf.knnMatch(query_des, train_des, k=2)
     # Lowe's ratio test
-    matches_good = [m1 for m1, m2 in matches if m1.distance < 0.75*m2.distance]
+    matches_good = [m1 for m1, m2 in matches if m1.distance < 0.75 * m2.distance]
+    query_idx = [i.queryIdx for i in matches_good]
+    train_idx = [i.trainIdx for i in matches_good]
+    p1 = np.float32([query_kp[i].pt for i in query_idx])
+    p2 = np.float32([train_kp[i].pt for i in train_idx])
 
-    return matches_good
+    return p1, p2
+
+def find_E_with_RANSAC(p1, p2):
+    # p1, p2 -> call by reference
+    E, mask = cv2.findEssentialMat(p1, p2, method=cv2.RANSAC)
+    p1 = p1[mask.ravel() == 1]
+    p2 = p2[mask.ravel() == 1]
+    
+    return E
 
 def main():
     pass
